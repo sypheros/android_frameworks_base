05f0b77694f660e305445225a3160ac5954daa49
Ardjlon <carlosardany@gmail.com>
2023-05-17 19:51:15 +0530

    Revert "libcameraservice: Add support to set vendor tag with client package name"
    This reverts commit cca27b06387aaa4bc816ec24d885a0a5657ebeb4.

--- services/camera/libcameraservice/CameraService.h
+++ services/camera/libcameraservice/CameraService.h
@@ -265,8 +265,6 @@ public:
     // Shared utilities
     static binder::Status filterGetInfoErrorCode(status_t err);
 
-    static std::string getCurrPackageName();
-
     /////////////////////////////////////////////////////////////////////
     // CameraClient functionality
 
